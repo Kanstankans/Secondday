7434102cdeac8512a8853f901a1896b2a7524d4b
kanstankans <kanstan6@gmail.com>
2021-07-25 12:58:30 +0530

    Wait

---
+++ src/org/test/Studend.java
@@ -0,0 +1,19 @@
+package org.test;
+
+
+
+public class Studend {
+
+		private void tst1() {
+			System.out.println("im no 1");
+		}
+		private void tst2() {
+			System.out.println("ïm no 2");
+		}
+		public static void main(String[] args) {
+			Studend s=new Studend();
+			s.tst1();
+			s.tst2();
+		}
+
+}
